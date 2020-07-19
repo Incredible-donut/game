875ad4eab214464b460d7db55ecf7789721f3f7a
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-07-19 15:04:50 +0400

    var clientMessage = ''; var clientName = '';

--- SendLetterEng.js
+++ SendLetterEng.js
@@ -6,23 +6,22 @@ var sendBtn = document.getElementById("send-btn");
 
 sendBtn.addEventListener('click', sendLetter);
 
-var message;
-var name;
+var clientMessage = '';
+var clientName = '';
 
 update();
 
 async function update(){
-  var name = nameVal.value;
-  var message = 'Email: ' + emailVal.value + 'Letter: ' + letterVal.value;
+  var clientName = nameVal.value;
+  var clientMessage = 'Email: ' + emailVal.value + 'Letter: ' + letterVal.value + 'Lang: En';
   update = setInterval(update, 500);
 }
  function sendLetter(){
  fetch('https://fchatiavi.herokuapp.com/send/iytter', {
    method: 'POST',
    body: JSON.stringify({
-   Name: name,
-   Message: message
+   Name: clientName,
+   Message: clientMessage
       })
     });
 }
-/*fetch(`https://fchatiavi.herokuapp.com/get/iytter/?offset=0&limit=10000`) */
